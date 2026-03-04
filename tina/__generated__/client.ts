326b26f50ba5196b0deaf2372bd88bb29aa3c6da
mituvladlen <vladlen.mitu@gmail.com>
2026-03-04 15:05:13 +0200

    first commit

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/Vladlen/Desktop/Univer/mediagrowth/tina/__generated__/.cache/1772622339857', url: 'https://content.tinajs.io/2.2/content/253b5243-c69f-4ab8-998f-fb2c5f7bed0d/github/main', token: 'c2f7f2a8da97e50936cf12265a572f44b4ed700f', queries,  });
 export default client;
   
